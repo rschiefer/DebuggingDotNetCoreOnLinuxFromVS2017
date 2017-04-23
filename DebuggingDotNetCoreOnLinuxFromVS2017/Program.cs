c979afc6f070231dcdbc13418dae603123e465dd
Robb Schiefer <rschiefer@live.com>
2017-04-23 07:29:16 -0500

    Add project files.

---
+++ DebuggingDotNetCoreOnLinuxFromVS2017/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DebuggingDotNetCoreOnLinuxFromVS2017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var message = $"Hello {name} from {System.Environment.MachineName}!";
+            Console.WriteLine(message);
+            
+            Console.WriteLine("Where are you from?");
+            var from = Console.ReadLine();
+            var fromMessage = $"{from} sounds like a cool place.";
+            Console.WriteLine(fromMessage);
+
+            Console.ReadLine();
+        }
+    }
+}
